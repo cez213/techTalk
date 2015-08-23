b0b06580ffbc94f478a45eb418c4c8a93c841637
Carolyn Zelenetz <carolyn.zelenetz@gmail.com>
2015-08-23 19:57:43 -0400

    updated contact page with iframe

--- routes/index.js
+++ routes/index.js
@@ -10,26 +10,8 @@ router.get('/input', function(req, res, next){
 	res.render('input');
 })
 
-/*router.put('/input/submit', function(req, res, next){
-
-	console.log("req", req);
-	res.json(req.params);
-})*/
-
-router.post('http://localhost:3000/input?firstName=Cooper&lastName=The+Hacker&email=cooper%40hackerdogs.com&password=hacker&photo=http%3A%2F%2Fpetdiys.com%2Fwp-content%2Fuploads%2F2014%2F03%2FDIY-Dog-Bandit-Mask.jpg&isAdmin=true', function(req, res, next){	
-	res.json(req.query);
-})
-
 router.get('/contact', function(req, res, next){
 	res.render('contact');
 })
-
-/*router.post('http://localhost:1337/api/account', function(req, res, next){
-	res.json()
-})
-
-router.post('http://localhost:1337/api/account', function(req, res, next){
-	res.json()
-})*/
 
 module.exports = router;
